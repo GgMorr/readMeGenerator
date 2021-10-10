b87303c733955879f440587a724127568900333b
Batya <Gigi.ucla21@gmail.com>
2021-10-10 16:15:14 -0700

    add license links

---
+++ README.md
@@ -0,0 +1,36 @@
+# ReadMe Template
+
+#Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Use](#use)
+* [License](#license)
+* [Contribution](#contribution)
+* [GitHub User Name](#user)
+* [About](#about)
+* [Email] (#email)
+
+
+## DESCRIPTION
+A template used to created a formal ReadMe file.
+
+## INSTALLATION
+Clone from Github
+
+## USE
+Clone to local repository, answer questions and run app.
+
+## LICENSE
+Apache license
+
+## CONTRIBUTION
+undefined
+
+## GITHUB USER NAME
+gg
+
+## ABOUT
+undefined
+
+### EMAIL
+gg@email.com
--- index.js
+++ index.js
@@ -91,8 +91,7 @@ inquirer
         type: 'list',
         name: 'license',
         message: 'Select your choice of license:',
-        choices: ['MIT ', 'Apache license', 'GPL license', 'Other'],
-           
+        choices: ['MIT license', 'Apache license', 'GPLv3 license'],  
         validate: licenseInput => {
             if (licenseInput) {
                 return true;
@@ -104,31 +103,51 @@ inquirer
   ])
   
 
-.then(({title, description, installation, use, contribution, user, email, about}) => {
+.then(({title, description, installation, use, license, contribution, user, email, about}) => {
+
+  switch (license) {
+    case "MIT": 
+          badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      break;
+
+    case "Apache 2.0": 
+          badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    
+          case "GPL v3": 
+          badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+          
+    default:
+      break;
+  };
+
     const template = `# ${title}
+
+#Table of Contents
 * [Description](#description)
 * [Installation](#installation)
 * [Use](#use)
+* [License](#license)
 * [Contribution](#contribution)
 * [GitHub User Name](#user)
 * [About](#about)
 * [Email] (#email)\n
 
-#Table of Contents
 ## DESCRIPTION
 ${description}\n
 ## INSTALLATION
 ${installation}\n
 ## USE
 ${use}\n
+## LICENSE
+${license}\n
 ## CONTRIBUTION
 ${contribution}\n
-## GitHub User Name
+## GITHUB USER NAME
 ${user}\n
 ## ABOUT
 ${about}\n
-## CONTACT
-* Email: ${email}`;
+### EMAIL
+${email}`;
 
     createNewFile(title,template);
     } 
